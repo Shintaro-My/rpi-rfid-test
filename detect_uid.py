fc8ef8d1926c9f2cfffe6a72f351fce5fd52f3bc
Tsuden-Miyahara <miyahara.tsuden@outlook.jp>
2023-07-21 15:17:54 +0900

    update

--- detect_uid.py
+++ detect_uid.py
@@ -5,7 +5,8 @@ import time
 import sys
 import sqlite3
 from my_util import init_db, init_gpio, buzzer, led_green, led_red
-from pirc522 import RFID
+#from pirc522 import RFID
+from mfrc522_i2c import MFRC522
 import RPi.GPIO as GPIO
 
 DB_NAME = 'user.db'
@@ -21,8 +22,15 @@ def main():
     cur = conn.cursor()
 
     run = True
-    rdr = RFID()
-    util = rdr.util()
+    #rdr = RFID()
+    #util = rdr.util()
+    i2cBus = 1
+    i2cAddress = 0x28
+
+    # Create an object of the class MFRC522
+    MFRC522Reader = MFRC522(i2cBus, i2cAddress)
+    version = MFRC522Reader.getReaderVersion()
+    print(f'MFRC522 Software Version: {version}')
 
     try:
         init_db(conn, cur)
@@ -32,13 +40,14 @@ def main():
             global run
             print("\nCtrl+C captured, ending read.")
             run = False
-            rdr.cleanup()
+            #rdr.cleanup()
             sys.exit()
 
         signal.signal(signal.SIGINT, end_read)
 
-        print("Starting")
         while run:
+            
+            """
             rdr.wait_for_tag()
 
             (error, data) = rdr.request()
@@ -48,6 +57,28 @@ def main():
 
                 (error, uid) = rdr.anticoll()
                 if not error:
+                    _uid = '-'.join(['{:02x}'.format(u) for u in uid])
+                    print(f"ID: {_uid}")
+                    users = [v for v in cur.execute(f'SELECT * FROM Users WHERE UserId = "{_uid}"')]
+                    if len(users):
+                        _, name = users[0][:2]
+                        print(f'Welcome, "{name}" <ID: {_uid}>!')
+                        led_green()
+                        buzzer()
+                        time.sleep(5)
+                        led_red()
+                    else:
+                        buzzer(2)
+                        print('[!] Invalid User.')
+                    time.sleep(1)
+            """
+            
+            (status, backData, tagType) = MFRC522Reader.scan()
+            
+            if status == MFRC522Reader.MIFARE_OK:
+                print('=' * 10)
+                (status, uid, backBits) = MFRC522Reader.identify()
+                if status == MFRC522Reader.MIFARE_OK:
                     _uid = '-'.join(['{:02x}'.format(u) for u in uid])
                     print(f"ID: {_uid}")
                     users = [v for v in cur.execute(f'SELECT * FROM Users WHERE UserId = "{_uid}"')]
