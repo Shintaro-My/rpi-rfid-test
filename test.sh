d3f835a4d106b327a09687945795510a3fc267fa
Tsuden-Miyahara <miyahara.tsuden@outlook.jp>
2023-08-03 17:14:01 +0900

    update

--- test.sh
+++ test.sh
@@ -1,7 +1,19 @@
 echo 'started.'
-sleep 2
-echo 'in progress.'
-sleep 2
-echo 'in progress.'
+sleep 1
+echo 'in progress 1 / 8'
+sleep 1
+echo 'in progress 2 / 8'
+sleep 1
+echo 'in progress 3 / 8'
+sleep 1
+echo 'in progress 4 / 8'
+sleep 1
+echo 'in progress 5 / 8'
+sleep 1
+echo 'in progress 6 / 8'
+sleep 1
+echo 'in progress 7 / 8'
+sleep 1
+echo 'in progress 8 / 8'
 sleep 2
 echo 'finished.'
