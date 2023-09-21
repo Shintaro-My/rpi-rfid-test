37820a51cd8ad62ded622c32a88f61eaa88fa5ac
Tsuden-Miyahara <miyahara.tsuden@outlook.jp>
2023-09-21 13:57:09 +0900

    update

---
+++ shutdown.py
@@ -0,0 +1,21 @@
+
+import RPi.GPIO as GPIO
+import subprocess
+import time
+
+SHUTDOWN  = 15
+def check_shutdown_setup():
+    GPIO.setup(SHUTDOWN , GPIO.IN, pull_up_down=GPIO.PUD_UP)
+def check_shutdown():
+    if GPIO.input(SHUTDOWN) == 0:
+        GPIO.clenup()
+        cmd = "sudo shutdown -h now"
+        subprocess.call(cmd, shell=True)
+        while GPIO.input(SHUTDOWN) == 0:
+            time.sleep(.05)
+            
+if __name__ == '__main__':
+    check_shutdown_setup()
+    while True:
+        check_shutdown()
+        time.sleep(.05)
