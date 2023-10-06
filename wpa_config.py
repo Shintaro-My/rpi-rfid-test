ea79387c1cbee1d8c28ac5e488532803597de6db
Tsuden-Miyahara <miyahara.tsuden@outlook.jp>
2023-10-06 10:04:41 +0900

    update

--- wpa_config.py
+++ wpa_config.py
@@ -4,9 +4,10 @@ import subprocess
 def create_pass(ssid, passphrase):
     cp = subprocess.run(
         f'wpa_passphrase {ssid} {passphrase}',
+        shell=True,
         capture_output=True
     )
-    txt = cp.stdout[-1]
+    txt = cp.stdout.decode()[:-1]
     print(txt)
 
 if __name__ == '__main__':
