5ff32aecc3b4e574048944fa5aedbdc6f4cb7531
Tsuden-Miyahara <miyahara.tsuden@outlook.jp>
2023-10-06 10:32:07 +0900

    update

--- wpa_config.py
+++ wpa_config.py
@@ -3,14 +3,18 @@ import subprocess
 
 BLANK = '        '
 
-def create_pass(ssid, passphrase):
+def create_pass(ssid, passphrase, priority=None):
     cp = subprocess.run(
         f'wpa_passphrase {ssid} {passphrase}',
         shell=True,
         capture_output=True
     )
-    txt = cp.stdout.decode().strip()[:-1] + f'{BLANK}key_mgmt=WPA-PSK'
+    attr = ['key_mgmt=WPA-PSK']
+    if priority is not None:
+        attr += [f'priority={priority}']
+    txt = cp.stdout.decode().strip()[:-1] + '\n'.join([f'{BLANK}{a}' for a in attr])
     print(txt)
+    return txt
 
 def splitter(ary):
     for i in range(0, len(ary), 2):
@@ -21,6 +25,6 @@ if __name__ == '__main__':
     n = len(sys.argv)
     if n % 2 == 0 or n == 1:
         raise Exception('引数の数が不正です。[ssid1, pass1, ssid2, pass2, ...]のように入力してください。')
-    [create_pass(ssid, passphrase) for ssid, passphrase in splitter(sys.argv[1:])]
+    [create_pass(*sp, priority=i) for i, sp in enumerate(splitter(sys.argv[1:]))]
 
 
